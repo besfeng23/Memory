0f64596681359930b35513cb3c7710299f0968e3
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-06-24 18:56:58 +0800

    chore: remove unused smoke rpc

--- supabase/migrations/20260624000100_first_live_append_rpc.sql
+++
@@ -1,9 +0,0 @@
-create or replace function public.first_live_append_proof_smoke()
-returns jsonb
-language sql
-security invoker
-as $$
-  select jsonb_build_object('ok', true, 'appendOnly', true);
-$$;
-
-grant execute on function public.first_live_append_proof_smoke() to authenticated;
